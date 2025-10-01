2520effb90c11e0fa77ab6f79002ecc2c90433c7
Nex-frontend <odraude28@live.com.mx>
2025-10-01 12:06:50 -0600

    css boton
    1.- botón: cambie de color y posición de texto a todas las páginas que usan botón
    2.- Cambie de color el Menú principal

--- fovisste/admin.py
+++ fovisste/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Record, Activity
 
+# Configuración de la aplicación
 @admin.register(Record)
 class RecordAdmin(admin.ModelAdmin):
     list_display = ("rfc", "nombre", "cadena1", "tipo", "impor", "cpto", "lote_actual", "qna", "ptje", "creado_en")
     search_fields = ("rfc", "nombre", "cadena1", "qna", "observacio", "lote_anterior", "qna_ini")
 
+
+# Configuración de la aplicación
 @admin.register(Activity)
-class ActivityAdmin(admin.ModelAdmin):
+class ActivityAdmin(admin.ModelAdmin): 
     list_display = ("user", "segmento", "actividad", "creado_en")
     search_fields = ("user__username", "segmento", "actividad")
